a1cc377a45d5c9202afe55ea9bb4d164bab64b98
Paulsaac <paulsaac@gmail.com>
2026-08-20 16:48:51 -0400

    feat: soporte para SECRETS_BASE64 en environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,12 @@ set -e
 
 mkdir -p /app/.streamlit
 
-# Si la variable de entorno SECRETS_TOML contiene credenciales, crear el archivo
-if [ -n "$SECRETS_TOML" ]; then
+# Si viene en Base64 (1 sola linea limpia para .env)
+if [ -n "$SECRETS_BASE64" ]; then
+    echo "$SECRETS_BASE64" | base64 -d > /app/.streamlit/secrets.toml
+elif [ -n "$SECRETS_TOML" ]; then
     printf "%s\n" "$SECRETS_TOML" > /app/.streamlit/secrets.toml
 fi
 
-# Iniciar la aplicación Streamlit
 exec streamlit run app.py --server.port=8501 --server.address=0.0.0.0
+
